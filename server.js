e69af00a1908dc24005f3eb39f5a4d7f867f0951
s-sakuda <sakuch03@gmail.com>
2022-01-09 15:55:55 +0900

    cors修正

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 
+if (process.env.NODE_ENV !== 'production') {
+	app.use(cors())
+}
+
 app.use(serveStatic(__dirname + '/dist'))
-app.use(cors())
 
 app.get('/api/hello', (req, res) => {
 	res.send('hello i am express')
